5c5854508f1c6f438d8b0c19af0fb5e2b6eeec14
muzhailong <1993414600@qq.com>
2017-11-05 23:37:19 +0800

    exgcd解不定方程

--- exgcd2/main.cpp
+++ exgcd2/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
+int exgcd(int a, int b, int &x, int &y)
+{
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+
+    int r = exgcd(b, a % b, x, y);
+    int temp = x;
+    x = y;
+    y = temp - r / b * y;
+    return r;
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    cin >> a >> b;
+    int x, y;
+
+    cout << exgcd(a, b, x, y);
+
+
     return 0;
 }
