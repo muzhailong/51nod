180e9052dedd3447db1af3fd9e4bbc13cbef8e88
muzhailong <1993414600@qq.com>
2017-11-05 23:23:32 +0800

    不定方程的exgcd解法

---
+++ exgcd2/cmake-build-debug/CMakeFiles/exgcd2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exgcd2.dir/main.cpp.obj"
+  "exgcd2.pdb"
+  "exgcd2.exe"
+  "exgcd2.exe.manifest"
+  "libexgcd2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exgcd2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exgcd2/cmake-build-debug/CMakeFiles/exgcd2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for exgcd2.
+# This may be replaced when dependencies are built.
